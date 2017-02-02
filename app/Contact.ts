5b5aaa7a2bb0f975083d2d7bc1a3738b9343ea95
nachomiralles <nachomiralles@gmail.com>
2017-02-02 10:34:55 +0100

    Basic APP

--- app/Contact.ts
+++ app/Contact.ts
@@ -1,3 +1,22 @@
-/**
- * Created by NachoGeotec on 30/01/2017.
- */
+export class Contact {
+
+  nif: number;
+  name: string;
+  surname: string;
+
+  constructor();
+  constructor(contact: Contact);
+  constructor(object: string);
+
+  constructor(contact?: Contact|string) {
+    if(!contact) return;
+    const temp = (typeof contact == "string")?JSON.parse(contact):contact;
+
+    this.nif = temp.nif?temp.nif:null;
+    this.name =  temp.name?temp.name:null;
+    this.surname =  temp.surname?temp.surname:null;
+
+  }
+
+
+}
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,10 +1,12 @@
 import { NgModule }      from '@angular/core';
+import { FormsModule }   from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import {HttpModule} from '@angular/http';
 
 import { AppComponent }  from './app.component';
 
 @NgModule({
-  imports:      [ BrowserModule ],
+  imports:      [ BrowserModule, HttpModule, FormsModule ],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
